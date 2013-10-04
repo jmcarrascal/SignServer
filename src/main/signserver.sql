856571dc32c5a3ad5694b176c1c93518b88e52ed
Juan Manuel Carrascal <jmcarrascal@gmail.com>
2013-10-04 17:30:03 -0400

    import data

---
+++ src/main/signserver.sql
@@ -0,0 +1,66 @@
+/*
+SQLyog Community v10.2 
+MySQL - 5.1.71-community : Database - signserver
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`signserver` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+/*Data for the table `certificadoconfiable` */
+
+LOCK TABLES `certificadoconfiable` WRITE;
+
+insert  into `certificadoconfiable`(`idCertificadoConfiable`,`certificado`,`descrip`) values (1,'0Ç»0Ç∞†0\r	*ÜHÜ˜\r\00K10	UAR1)0\'U\n Infraestructura de Firma Digital10UAC Ra√≠z0\r101025151056Z\r201022151056Z0ÇL10	UAR1)0\'U Ciudad Aut√≥noma de Buenos Aires1301U*Subsecretar√≠a de Tecnolog√≠as de Gesti√≥n1)0\'U Secretar√≠a de Gesti√≥n P√∫blica1907U0Oficina Nacional de Tecnolog√≠as de Informaci√≥n1*0(U\n!Jefatura de Gabinete de Ministros10UCUIT 30680604572100.U\'Autoridad Certificante de Firma Digital0Ç\"0\r	*ÜHÜ˜\r\0Ç\00Ç\nÇ\0ﬁÀµ≥L)Kµ∫[≤¶œßµZÊzjJGoÒO⁄g˚åÌ‡Ù–´∞F]≥ @ÂQë^êIä„2>’®5…É´⁄ âË¨¿⁄∂f±¢˜ùµà‰ˇãÉ$µJ/{V>æJñ\\‚ÑÔ;¿A√ù»^∫Ÿ≠	ÓB†å™H˝HóÏ\np—zvSºí£†Eù¢«Lüêw≠KßÌÒ ‰—\n•ñä\"<+ã*·„(Ñ3Ft\"xRÓî“üº?˙Ëg†¯‹E\Z…Ç˛xœ‡‘ò∂±⁄à¨ÁH⁄|;Õñ–\rìO%ø≠¸T°Ù}QBsGâ~∞ûíÚò;kHøÕ≥«_\r\0ND±ò˙ùŒlPJMÅû0.‰j,ÖÅ4\nSH1âtw˙R⁄•™0çÈx◊9Ô ¡˝Ø; ü>î}˚±⁄5£‰ßÂ˛‹æ≈Ôµà⁄Øn¿ªÁI7ÕfbÕÎäÕ†e[û≠≠s—@p/¨X8%˙˘ÅÁª˝*µŸf≥›ˆîÃ~‚*ÔÃÚ≥¬B¡Ê^Ï/ÓÏt∞˚,Ån?´[å#©Ñ® Ù^qN¶%\'€/wÌh¿’WNE÷I„Êãös∫_{Éºù˙ÿSyg>˚œs6Ö=ò]Á<>#,£4\0©É˛Q…MÄËÅg¬Ø(òKJÁ÷L√qAO¶	õ“V`¢≠˚Ê!É#s±§q\0£Ç≤0ÇÆ0Uˇ0ˇ\00U0U#0Ä]‹<1ÇüC[%`kG¿òtÅÂd0Uå˝.lpl3í\Zm$ÃõÃ?ô≈Î®0YUR0P0&†$†\"Ü http://acraiz.cdp1.gov.ar/ca.crl0&†$†\"Ü http://acraiz.cdp2.gov.ar/ca.crl0Å∂U ÅÆ0Å´0Å®` \00Åû0(+http://acraiz.gov.ar/cps.pdf0r+0f\ZdLey 25.506 - Infraestructura de Firma Digital de la Republica Argentina, Autoridad Certificante Raiz07++0)0\'+0Ühttp://acraiz.gov.ar/ca.crt0\r	*ÜHÜ˜\r\0Ç\0\ZÊBû–œ˘ÅÓ[b5¨jç¯WøÀHzP‰˙Ãçchµ;Ù’ZA©π=∫]∫a…f;ZDvÇ`T:ëp”ô\Z˚ÄèZq>õıyFˇQU\Z≤»åáŸ‹„A⁄˝…⁄”¡ZÎr\\¬W˚s8À.ƒv »Ùk\Z™ŸA>À;îıÿõ∂˚˘Ôbø≥ﬁA1G¨˝Ÿ&‹ó^1EJ|4ª ∫Á\\i\\]g;!îÖ\0Í67V>QªHæç◊å(\Z_\\:√¨∫¸∞¢œ∆mF®Øj’6Àx\\7]O\rû#.Ê8°Á3«yî⁄ö \\∑ÇST÷UıHÑﬂ⁄ñ_ùø\\@Ê4¶}dïc	√lä˙ı,è\'æïCY+ä9•:Ú\rOI˙jF 5.|‡ÂºﬁØÄ˛A∂[ê∞Å∞ÕÉçÕR#•Ï——˝*ﬂó—eÖ8J;r’t∞èà\Z//_ºPıDZÓ`(,ó ≠V8Ufˇ5rΩ:\\	´ÂçQê›…„æ“u©Î⁄…ÿ·Qãà˙\Zm?ﬂQ>ˆ3b∆Ëä£’¶Ç3]LÖ@ª&”‚ú\r¸„∞n»n∑∞ü—∑¿(≠§+J.)Ë£«Æ∏åÙûgñÀvkõ·}K¨€Ìãb%wU¡â|Ω%ÀvèÇ ‹> ƒ¬fvxÜ®ú‡°0Ïl}É®ÚÂjÔÔXFsS GD	8Ÿ!yG~uêp.–∏KDèXá','Certificado Intermedio ONTI');
+
+UNLOCK TABLES;
+
+/*Data for the table `firmante` */
+
+LOCK TABLES `firmante` WRITE;
+
+insert  into `firmante`(`idFirmante`,`locacion`,`passwordKeyStore`,`passwordTSA`,`razon`,`ultimoAcceso`,`urlTSA`,`usaTSA`,`userTSA`,`rutaKeyStore`) values (1,'Buenos Aires','123456','jmc1159','Ventanilla Electronica',NULL,'http://tsatest2.digistamp.com/tsa','','11295','c://ks3.p12');
+
+UNLOCK TABLES;
+
+/*Data for the table `mensaje` */
+
+LOCK TABLES `mensaje` WRITE;
+
+insert  into `mensaje`(`idMensaje`,`descrip`) values (0,'OK'),(1,'Formato de Documento PDF Invalido'),(2,'El documento no se encuentra firmado digitalmente'),(3,'No se pudo validar las firmas del documento'),(4,'Alguna de las firmas del documento no esta generarda a partir de un certificado digital v√É¬°lido'),(5,'No se recibi√≥ ning√∫n documento'),(6,'Error al invocar la TSA'),(7,'Error Interno del sistema al realizar el sello del documento'),(8,'Error al procesar el certificado Root de confianza asociado al usuario'),(9,'Error al no encontrar el certificado del firmante'),(10,'Error en la autenticacion'),(34,'werwr'),(42,'Aver como Funca'),(6575,'asasdasda'),(121212,'Jolla'),(23324324,'ssdfsfsdsdfsd');
+
+UNLOCK TABLES;
+
+/*Data for the table `parametrizacion` */
+
+LOCK TABLES `parametrizacion` WRITE;
+
+insert  into `parametrizacion`(`idParametrizacion`,`descrip`,`valor`) values (10,'Usa proxy','false'),(11,'Url Proxy','10.3.8.1'),(12,'Puerto Proxy','80');
+
+UNLOCK TABLES;
+
+/*Data for the table `usuario` */
+
+LOCK TABLES `usuario` WRITE;
+
+insert  into `usuario`(`idUsuario`,`activo`,`password`,`ultimoAcceso`,`userName`) values (1,'','user',NULL,'user');
+
+UNLOCK TABLES;
+
+/*Data for the table `usuariocertificado` */
+
+LOCK TABLES `usuariocertificado` WRITE;
+
+insert  into `usuariocertificado`(`idUsuarioCertificado`,`idCertificadoConfiable`,`idUsuario`) values (1,1,1);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
